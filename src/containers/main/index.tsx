269b64dc82cba2593ccc08589a8a4ca7457f04c1
Yuri Mustifaga <yurimusti@gmail.com>
2022-02-16 15:10:53 -0300

    fix: add idEmpresa

--- src/containers/main/index.tsx
+++ src/containers/main/index.tsx
@@ -11,7 +11,7 @@ import Zoom from "react-reveal/Zoom";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
-import { Modal, Button, notification } from 'antd';
+import { Modal, Button, notification } from "antd";
 
 import Loading from "../../components/Loading";
 
@@ -30,7 +30,7 @@ const Main = () => {
   const router = useRouter();
 
   const [createLandingpage, { data, loading: loadingCreate, error }] =
-  useMutation(CREATE_LEAD);
+    useMutation(CREATE_LEAD);
 
   const [email, setEmail] = useState("");
   const [nome, setNome] = useState("");
@@ -54,6 +54,7 @@ const Main = () => {
           message: "Sucesso",
           description: "Cadastro efetuado com sucesso.",
         });
+        handleCancel();
       } else {
         notification.error({
           message: "Error",
@@ -65,20 +66,22 @@ const Main = () => {
 
   const _handleCreate = () => {
     if (
-      nome ==="" ||
+      nome === "" ||
       email === "" ||
       celular === "" ||
       funcionarios === "" ||
-      instagram === ""      
+      instagram === ""
     ) {
       notification.warning({
         message: "Alerta",
-        description: "É necessário preencher todos os campos antes de continuar.",
+        description:
+          "É necessário preencher todos os campos antes de continuar.",
       });
     } else {
       createLandingpage({
         variables: {
           input: {
+            idEmpresa: "620c11f9169525a636b9d917",
             nome,
             email,
             celular,
@@ -90,101 +93,90 @@ const Main = () => {
     }
   };
 
-    return (
-      <Styled.MainBanner
-        justifyContent={["center", "center", "center"]}
-        backgroundImage={[
-          `url(${MainBg})`,
-          `url(${MainBg})`,
-          `url(${MainBg})`,
-        ]}
-      >
-        <Styled.LogoContainer>
-          <Fade left>
-            <Logo />
+  return (
+    <Styled.MainBanner
+      justifyContent={["center", "center", "center"]}
+      backgroundImage={[`url(${MainBg})`, `url(${MainBg})`, `url(${MainBg})`]}
+    >
+      <Styled.LogoContainer>
+        <Fade left>
+          <Logo />
+        </Fade>
+      </Styled.LogoContainer>
+
+      <Styled.TextContainer mx={[50, 50, 160]}>
+        <Styled.Title fontSize={[30, 40, 60]}>
+          <Fade left delay={100}>
+            Uma solução inteligente de marketing e publicidade para sua empresa!
           </Fade>
-        </Styled.LogoContainer>
+        </Styled.Title>
 
-        <Styled.TextContainer
-        mx={[50, 50, 160]}
-        >
-          <Styled.Title 
-          fontSize={[30, 40, 60]}
-          >
-            <Fade left delay={100}>
-              Uma solução inteligente de
-              marketing e publicidade
-              para sua empresa!
-            </Fade>
-          </Styled.Title>
-
-          <Styled.ButtonContainer>
-            <Fade left delay={200}>
-              <Styled.RegisterBtn
+        <Styled.ButtonContainer>
+          <Fade left delay={200}>
+            <Styled.RegisterBtn
               onClick={showModal}
               fontSize={[18, 24, 24]}
               py={[18, 25, 25]}
               width={[200, 300, 300]}
-              >
-                  SAIBA MAIS
-              </Styled.RegisterBtn>
-            </Fade>
-            <Modal 
-            title="Para ficar por dentro das novidades que vão acontecer, inscreva-se abaixo." 
-            visible={isModalVisible} 
-            onOk={_handleCreate} 
-            onCancel={handleCancel}
             >
-              <Styled.Form>
-                <Styled.InputContainer>
-                  Nome:
-                  <Styled.Input 
-                    placeholder="Seu nome..."
-                    type="name"
-                    value={nome}
-                    onChange={(e) => setNome(e.target.value)}
-                  />             
-                </Styled.InputContainer>
-                <Styled.InputContainer>
-                  Seu melhor email:
-                  <Styled.Input 
-                    placeholder="exemplo@email.com.br"
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />             
-                </Styled.InputContainer>
-                <Styled.InputContainer>
-                  Seu Whatsapp
-                  <Styled.Input 
-                    placeholder="(DDD) 99999-8888"
-                    value={celular}
-                    onChange={(e) => setCelular(e.target.value)}
-                  />             
-                </Styled.InputContainer>
-                <Styled.InputContainer>
-                  Numero de funcionários:
-                  <Styled.Input 
-                    placeholder="Ex: 48"
-                    value={funcionarios}
-                    onChange={(e) => setFuncionarios(e.target.value)}
-                  />             
-                </Styled.InputContainer>
-                <Styled.InputContainer>
-                  Instagram da empresa:
-                  <Styled.Input 
-                    placeholder="@suaempresa"
-                    value={instagram}
-                    onChange={(e) => setInstagram(e.target.value)}
-                  />             
-                </Styled.InputContainer>
-              </Styled.Form>
-            </Modal>
-
-          </Styled.ButtonContainer>
-        </Styled.TextContainer>
-      </Styled.MainBanner>
-    );
-  };
+              SAIBA MAIS
+            </Styled.RegisterBtn>
+          </Fade>
+          <Modal
+            title="Para ficar por dentro das novidades que vão acontecer, inscreva-se abaixo."
+            visible={isModalVisible}
+            onOk={_handleCreate}
+            onCancel={handleCancel}
+          >
+            <Styled.Form>
+              <Styled.InputContainer>
+                Nome:
+                <Styled.Input
+                  placeholder="Seu nome..."
+                  type="name"
+                  value={nome}
+                  onChange={(e) => setNome(e.target.value)}
+                />
+              </Styled.InputContainer>
+              <Styled.InputContainer>
+                Seu melhor email:
+                <Styled.Input
+                  placeholder="exemplo@email.com.br"
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Styled.InputContainer>
+              <Styled.InputContainer>
+                Seu Whatsapp
+                <Styled.Input
+                  placeholder="(DDD) 99999-8888"
+                  value={celular}
+                  onChange={(e) => setCelular(e.target.value)}
+                />
+              </Styled.InputContainer>
+              <Styled.InputContainer>
+                Numero de funcionários:
+                <Styled.Input
+                  placeholder="Ex: 48"
+                  value={funcionarios}
+                  onChange={(e) => setFuncionarios(e.target.value)}
+                />
+              </Styled.InputContainer>
+              <Styled.InputContainer>
+                Instagram da empresa:
+                <Styled.Input
+                  placeholder="@suaempresa"
+                  value={instagram}
+                  onChange={(e) => setInstagram(e.target.value)}
+                />
+              </Styled.InputContainer>
+            </Styled.Form>
+          </Modal>
+        </Styled.ButtonContainer>
+      </Styled.TextContainer>
+    </Styled.MainBanner>
+  );
+};
 
 export default Main;
